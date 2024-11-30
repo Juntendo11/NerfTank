eb4916dbdb310cb882209b25009fab7d96ed8c9e
Juntendo11 <86496557+Juntendo11@users.noreply.github.com>
2024-11-30 18:43:50 +1000

    Add files via upload

--- arduino/Tank/Tank.ino
+++ arduino/Tank/Tank.ino
@@ -5,6 +5,7 @@
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *********/
 
+#include <ESP32Servo.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
@@ -22,6 +23,12 @@
 #define UP_THRESHOLD    4000
 #define DOWN_THRESHOLD  6000
 
+
+#define LEFT_ANGLE_RANGE  170
+#define RIGHT_ANGLE_RANGE 10
+
+#define UP_ANGLE_RANGE    125
+#define DOWN_ANGLE_RANGE  145
 
 typedef struct struct_message {
   int Rx;
@@ -41,6 +48,14 @@ int motor1Pin4 = 9;
 int enableA = 11; //1,2
 int enableB = 12; //3,4
 
+int servoPin1 = 39;
+int servoPin2 = 37;
+
+int servoHoriAng = 90;  
+int servoVertAng = 135; //30 degrees of angle of freedom
+
+Servo servoHori;  // create servo object to control a servo
+Servo servoVert;  // create servo object to control a servo
 
 // Setting PWM properties
 const int freq = 30000;
@@ -48,9 +63,12 @@ const int pwmChannel = 0;
 const int resolution = 8;
 int dutyCycle = 255;
 
-void idle() {
+Servo myservo;  // create servo object to control a servo
+const int servoSpeed = 3;
+
+void move_idle() {
   //Stop
-  Serial.println("Stop");
+  Serial.println("Idle");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor1Pin3, LOW);
@@ -89,7 +107,25 @@ void turn_left() {
   digitalWrite(motor1Pin4, LOW);
 }
 
+void turret_idle(){
+  Serial.println("Turret idle");
+}
 
+void turret_up(){
+  Serial.println("Turret Up");
+}
+
+void turret_down(){
+  Serial.println("Turret Down");
+}
+
+void turret_right(){
+  Serial.println("Turret Right");
+}
+
+void turret_left(){
+  Serial.println("Turret Left");
+}
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
@@ -117,7 +153,12 @@ void setup() {
   // configure LEDC PWM
   ledcAttachChannel(enableA, freq, resolution, pwmChannel);
   ledcAttachChannel(enableB, freq, resolution, pwmChannel);
-
+  
+  //Servo config
+  servoHori.attach(servoPin1);
+  servoVert.attach(servoPin2);
+  servoHori.write(servoHoriAng);
+  servoVert.write(servoVertAng);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   // Init ESP-NOW
@@ -138,28 +179,64 @@ void loop() {
   ledcWrite(enableA, dutyCycle);
   ledcWrite(enableB, dutyCycle);
 
-  // check left/right commands
-  if (myData.Rx < LEFT_THRESHOLD) {
-    Serial.println(myData.Rx);
-    turn_left();
+  //If in move mode
+  if (myData.turretMode == 0) {
+    // check left/right commands
+    if (myData.Rx < LEFT_THRESHOLD) {
+      Serial.println(myData.Rx);
+      turn_left();
+    }
+    else if (myData.Rx > RIGHT_THRESHOLD) {
+      turn_right();
+    }
+    // check up/down commands
+    else if (myData.Ry < UP_THRESHOLD) {
+      move_forwards();
+    }
+    else if (myData.Ry > DOWN_THRESHOLD) {
+      move_backwards();
+    }
+    else {
+      move_idle();
+    }
   }
-  else if (myData.Rx > RIGHT_THRESHOLD) {
-    turn_right();
-  }
-  // check up/down commands
-  else if (myData.Ry < UP_THRESHOLD) {
-    move_forwards();
-  }
-  else if (myData.Ry > DOWN_THRESHOLD) {
-    move_backwards();
-  }
+  //If in Turret mode
   else {
-    idle();
+    if (myData.Rx < LEFT_THRESHOLD) {
+      if (servoHoriAng > LEFT_ANGLE_RANGE){
+        servoHoriAng += servoSpeed;
+      }
+      servoHori.write(servoHoriAng);
+      Serial.println(servoHoriAng);
+      turret_left();
+    }
+    else if (myData.Rx > RIGHT_THRESHOLD) {
+      if (servoHoriAng > RIGHT_ANGLE_RANGE){
+        servoHoriAng -= servoSpeed;
+      }
+      servoHori.write(servoHoriAng);
+      Serial.println(servoHoriAng);
+      turret_right();
+    }
+    // check up/down commands
+    else if (myData.Ry < UP_THRESHOLD) {
+      servoVertAng += servoSpeed;
+      servoVert.write(servoVertAng);
+      Serial.println(servoVertAng);
+      turret_up();
+    }
+    else if (myData.Ry > DOWN_THRESHOLD) {
+      servoVertAng -= servoSpeed;
+      servoVert.write(servoVertAng);
+      Serial.println(servoVertAng);
+      turret_down();
+    }
+    else {
+      turret_idle();
+    }
   }
 
-  //if ((LEFT_THRESHOLD < myData.Rx <RIGHT_THRESHOLD)&&(DOWN_THRESHOLD < myData.Ry <UP_THRESHOLD)){
-  //idle();
-  //}
+
   delay(100);
 
 
